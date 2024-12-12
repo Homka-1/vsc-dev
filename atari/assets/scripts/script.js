440a425e3a05aeffdf73aef0cc9e1581b0ca0ed7
homka <egor98108@gmail.com>
2024-12-12 21:57:25 +0500

    added ball movement

--- atari/assets/scripts/script.js
+++ atari/assets/scripts/script.js
@@ -1,3 +1,50 @@
+//board
+let board = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+//ball
+const ball = document.querySelector('.ball')
+
+let ballArray= {
+    x : board.width / 2 + 0.5,
+    y : board.height / 2 + 0.5,
+    dx : 1,
+    dy : -2
+}
+
+//platform
+const platform = document.querySelector('.platform');
+
+
+function ballMove() {
+    setInterval(() => {
+        ball.style.left = ballArray.x + 'px'
+        ball.style.bottom = ballArray.y + 'px'
+        ballArray.x += ballArray.dx
+        ballArray.y += ballArray.dy
+        if (ballArray.y <= 0 || (ballArray.y + ball.style.height >= board.height)) {
+            ballArray.dy *= -1;
+        }
+        if (ballArray.x <= 0 || (ballArray.x + ball.style.width >= board.width)) {
+            ballArray.dx *= -1;
+        }
+        if (ball.style.left >= platform.style.left && ball.style.left <= platform.style.left + platform.style.width && ball.style.bottom + ball.style.height === '120px') {
+            ballArray.dy *= -1;
+            ballArray.dx *= -1;
+        }
+        if (ball.style.left === platform.style.left) {
+            console.log('YEAH');
+        }
+    }, 2);
+}
+
+
+
+
+ballMove();
+
 const keyOutput = document.getElementById('keyOutput');
 document.addEventListener('keydown', function(event) {
     const keyPressed = event.key;
@@ -7,7 +54,7 @@ document.addEventListener('keydown', function(event) {
 let platformMoveInterval;
 
 window.onkeydown = function(event) {
-    const platform = document.querySelector('.platform');
+
 
     if (platformMoveInterval) return;
 
